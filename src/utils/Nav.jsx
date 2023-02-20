b7e3ceba58eda38133ee0f79be99563529e400c2
KoichiDevs <lista.officialph@gmail.com>
2023-02-20 15:00:14 +0800

    added position fixed

--- src/utils/Nav.jsx
+++ src/utils/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
     }
 
     return (
-        <motion.nav initial={{ opacity: 0 }} animate={{ opacity: 100 }} transition={{ duration: 0.7 }} className='w-full h-full pb-10 lg:h-auto px-10 py-7 text-white absolute z-20 top-0 left-0 font-sharp'>
+        <motion.nav initial={{ opacity: 0 }} animate={{ opacity: 100 }} transition={{ duration: 0.7 }} className='w-full h-full pb-10 lg:h-auto px-10 py-7 text-white absolute z-20 top-0 left-0 font-sharp' style={active ? { position: "fixed" } : {}}>
             <div className="max-w-[1700px] w-full h-auto flex items-center justify-center mx-auto">
                 <ul className='items-center gap-x-12 flex lg:relative absolute right-0 lg:w-auto lg:h-auto w-full h-full top-0 justify-center lg:justify-center flex-col lg:flex-row bg-black lg:bg-transparent gap-y-20 text-xl lg:text-sm opacity-0 transition-all ease-in-out duration-300 lg:opacity-100 lg:pointer-events-auto pointer-events-none' style={active ? { opacity: "100%", pointerEvents: "auto" } : {}}>
                     <li className='absolute lg:hidden block text-4xl top-10 right-10'><RiCloseFill onClick={setToggle} /></li>
@@ -29,7 +29,7 @@ const Nav = () => {
                     })}
                 </ul>
 
-                <motion.img initial={{width: "0rem"}} animate={{width: "25rem"}} transition={{duration: 0.7}} src="/line.webp" alt="line" className="absolute bottom-[-1rem] lg:block hidden pointer-events-none w-[25rem]" />
+                <motion.img initial={{ width: "0rem" }} animate={{ width: "25rem" }} transition={{ duration: 0.7 }} src="/line.webp" alt="line" className="absolute bottom-[-1rem] lg:block hidden pointer-events-none w-[25rem]" />
 
                 <img src="/logo.webp" alt="Logo" className="w-[5rem] lg:absolute top-32 2xl:w-[6.5rem]" />
 
